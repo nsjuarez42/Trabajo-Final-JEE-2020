d6a8858901053d023edd68e87678d7a3aa217d71
nsjuarez42 <63726225+nsjuarez42@users.noreply.github.com>
2020-12-22 18:59:37 -0300

    correcciones

--- buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.example.ahorcado/Ahorcado .
-call docker rm -f Ahorcado
-call docker run -d -p 9080:9080 -p 9443:9443 --name Ahorcado com.example.ahorcado/Ahorcado
